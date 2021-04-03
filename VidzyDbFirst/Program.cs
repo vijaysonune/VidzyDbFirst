ac82921565ec98dd346cc29f2064baeebb6638cd
vsonune <vsonune@EC2AMAZ-S4GP8VO>
2021-04-03 19:47:27 +0530

    Add project files.

---
+++ VidzyDbFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidzyDbFirst
+{
+    enum ClassificationVideo
+    {
+        Gold=1,
+        Silver=2,
+        Platinium=3,
+        Premium=4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context =new VidzyDbContext();
+            Video video = new Video();
+            video.Name = "";
+            
+            context.AddVideo("Race 2", DateTime.Now, "Action", 3);
+        }
+    }
+}
